4501e75548c281ddac9c312af572edfcc270be05
Arya Yudha P <aryayudha2109@gmail.com>
2025-02-26 20:08:16 +0700

    Menambahkan Form Username yang akan ditampilkan di halaman Welcome

--- app/src/main/java/dev/yudha/projectindividu/MainActivity.kt
+++ app/src/main/java/dev/yudha/projectindividu/MainActivity.kt
@@ -2,8 +2,10 @@ package dev.yudha.projectindividu
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -11,17 +13,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val editTextUsername = findViewById<EditText>(R.id.editTextUsername)
         val editTextEmail = findViewById<EditText>(R.id.editTextEmail)
         val editTextPassword = findViewById<EditText>(R.id.editTextPassword)
         val buttonLogin = findViewById<Button>(R.id.buttonLogin)
 
         buttonLogin.setOnClickListener {
+            val username = editTextUsername.text.toString()
             val email = editTextEmail.text.toString()
             val password = editTextPassword.text.toString()
 
-            if (email.isNotEmpty() && password.isNotEmpty()) {
+            if (username.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()) {
+                // Log data ke Logcat
+                Log.d("LoginInfo", "Username: $username")
+                Log.d("LoginInfo", "Email: $email")
+                Log.d("LoginInfo", "Password: $password") // Jangan gunakan ini untuk aplikasi produksi!
+
+                // Pindah ke halaman Welcome
                 val intent = Intent(this, WelcomeActivity::class.java)
+                intent.putExtra("USERNAME", username)
                 startActivity(intent)
+            } else {
+                Toast.makeText(this, "Harap isi semua kolom!", Toast.LENGTH_SHORT).show()
             }
         }
     }
--- app/src/main/java/dev/yudha/projectindividu/WelcomeActivity.kt
+++ app/src/main/java/dev/yudha/projectindividu/WelcomeActivity.kt
@@ -1,11 +1,17 @@
 package dev.yudha.projectindividu
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        val textViewWelcome = findViewById<TextView>(R.id.textViewWelcome)
+        val username = intent.getStringExtra("USERNAME")
+
+        textViewWelcome.text = "Selamat Datang, $username!"
     }
 }

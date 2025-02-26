fec5ca002ab7bbd5b66be6ffc5ae597ce8d72c9e
Arya Yudha P <aryayudha2109@gmail.com>
2025-02-26 23:37:13 +0700

    Merapikan tampilan

--- app/src/main/java/dev/yudha/projectindividu/WelcomeActivity.kt
+++ app/src/main/java/dev/yudha/projectindividu/WelcomeActivity.kt
@@ -1,7 +1,8 @@
 package dev.yudha.projectindividu
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class WelcomeActivity : AppCompatActivity() {
@@ -9,9 +10,14 @@ class WelcomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
-        val textViewWelcome = findViewById<TextView>(R.id.textViewWelcome)
-        val username = intent.getStringExtra("USERNAME")
+        val buttonLogout = findViewById<Button>(R.id.button)
 
-        textViewWelcome.text = "Selamat Datang, $username!"
+        // Fungsi tombol Logout untuk kembali ke halaman Login
+        buttonLogout.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Menutup WelcomeActivity agar tidak bisa kembali dengan tombol back
+        }
     }
 }
+
